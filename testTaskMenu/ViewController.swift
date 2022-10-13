91c3908728feae115d25d8163ca3a4e33ec3fb98
David Tonoyan <davidtonoyan10@me.com>
2022-10-13 17:50:51 +0400

    Project initialization

--- testTaskMenu/ViewController.swift
+++ testTaskMenu/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let request = NSMutableURLRequest(url: NSURL(string: reqUrl)! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print("OK")
+                print(httpResponse)
+            }
+        })
+
+        dataTask.resume()
     }
 
+    let reqUrl = "https://burger-king-menu.p.rapidapi.com/categories"
 
+    let headers = [
+        "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+        "X-RapidAPI-Host": "burger-king-menu.p.rapidapi.com"
+    ]
 }
 
