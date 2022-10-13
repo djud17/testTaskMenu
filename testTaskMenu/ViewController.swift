0cf93eef88c13422b68d66db8f7531e4bdaccda4
David Tonoyan <davidtonoyan10@me.com>
2022-10-13 15:30:39 +0400

    Initial Commit

---
+++ testTaskMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTaskMenu
+//
+//  Created by Давид Тоноян  on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
