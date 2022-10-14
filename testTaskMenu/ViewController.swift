75bb09d19558fb25341810db0c4e2c6db5d18452
David Tonoyan <davidtonoyan10@me.com>
2022-10-14 19:57:59 +0400

    Add TableView & CollectionView

--- testTaskMenu/ViewController.swift
+++ testTaskMenu/ViewController.swift
@@ -10,17 +10,24 @@ import SnapKit
 import Kingfisher
 
 final class MenuViewController: UIViewController {
-    private var menuTableView: UITableView = {
-        return UITableView()
-    }()
     private var advertBackView: UIView = {
         let view = UIView()
-        view.layer.borderWidth = 3
-        view.layer.borderColor = UIColor.black.cgColor
         return view
     }()
-    
     private var advertCollectionView: UICollectionView!
+    
+    private var filtersBackView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private var filtersCollectionView: UICollectionView!
+    private var selectedFilterId = 1
+    
+    private var menuTableView: UITableView = {
+        let tableVIew = UITableView()
+        return tableVIew
+    }()
+    
     private let apiClient: ApiClient = ApiClientImpl()
     var productCategories: [ProductCategory]?
     
@@ -28,7 +35,7 @@ final class MenuViewController: UIViewController {
         super.viewDidLoad()
         
         setupView()
-        //loadData()
+        loadData()
     }
     
     private func loadData() {
@@ -41,6 +48,8 @@ final class MenuViewController: UIViewController {
                 case .success(let categories):
                     self.productCategories = categories
                 }
+                self.menuTableView.reloadData()
+                self.filtersCollectionView.reloadData()
             }
         }
     }
@@ -49,6 +58,8 @@ final class MenuViewController: UIViewController {
         view.backgroundColor = .white
         
         setupAdvertBlock()
+        setupFiltersView()
+        setupTableView()
     }
     
     private func setupAdvertBlock() {
@@ -65,7 +76,7 @@ final class MenuViewController: UIViewController {
         layout.scrollDirection = .horizontal
         
         advertCollectionView = UICollectionView(frame: CGRect(), collectionViewLayout: layout)
-        advertCollectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "advertCell")
+        advertCollectionView.register(AdvertCollectionViewCell.self, forCellWithReuseIdentifier: "advertCell")
         advertCollectionView.backgroundColor = .white
         advertCollectionView.alwaysBounceHorizontal = true
         advertCollectionView.dataSource = self
@@ -73,25 +84,136 @@ final class MenuViewController: UIViewController {
         advertBackView.addSubview(advertCollectionView)
         
         advertCollectionView.snp.makeConstraints { make in
-            make.top.leading.trailing.bottom.equalToSuperview().inset(10)
+            make.leading.trailing.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    private func setupFiltersView() {
+        view.addSubview(filtersBackView)
+        
+        filtersBackView.backgroundColor = .white
+        
+        filtersBackView.snp.makeConstraints { make in
+            make.top.equalTo(advertBackView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(80)
+        }
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 150, height: 40)
+        layout.scrollDirection = .horizontal
+
+        filtersCollectionView = UICollectionView(frame: CGRect(), collectionViewLayout: layout)
+        filtersCollectionView.register(FilterCollectionViewCell.self, forCellWithReuseIdentifier: "filterCell")
+        filtersCollectionView.backgroundColor = .white
+        filtersCollectionView.alwaysBounceHorizontal = true
+        filtersCollectionView.dataSource = self
+
+        filtersBackView.addSubview(filtersCollectionView)
+
+        filtersCollectionView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(10)
         }
     }
     
     private func setupTableView() {
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
+        menuTableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "menuCell")
+        menuTableView.backgroundColor = .white
+        menuTableView.allowsSelection = false
+        menuTableView.separatorColor = .white
+        view.addSubview(menuTableView)
         
+        menuTableView.snp.makeConstraints { make in
+            make.top.equalTo(filtersBackView.snp.bottom).offset(10)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    @objc private func filterButtonTapped(sender: UIButton) {
+        selectedFilterId = sender.tag
+        filtersCollectionView.reloadData()
+    }
+    
+    private func setButtonColor(_ button: UIButton?) {
+        if let button = button {
+            let buttonColor = UIColor(red: 0.992, green: 0.227, blue: 0.412, alpha: 0.2)
+            button.layer.borderColor = buttonColor.cgColor
+            button.setTitleColor(buttonColor, for: .highlighted)
+            
+            if button.tag == selectedFilterId {
+                button.backgroundColor = buttonColor
+                button.setTitleColor(buttonColor.withAlphaComponent(1), for: .normal)
+            } else {
+                button.backgroundColor = .white
+                button.setTitleColor(buttonColor, for: .normal)
+            }
+        }
     }
 }
 
 extension MenuViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        3
+        collectionView == self.advertCollectionView ? 3 : productCategories?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "advertCell",
-                                                      for: indexPath) as? CustomCollectionViewCell
-        cell?.imageView.image = UIImage(named: "advert")
-        return cell ?? UICollectionViewCell()
+        if collectionView == self.advertCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "advertCell",
+                                                          for: indexPath) as? AdvertCollectionViewCell
+            cell?.imageView.image = UIImage(named: "advert")
+            return cell ?? UICollectionViewCell()
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "filterCell",
+                                                          for: indexPath) as? FilterCollectionViewCell
+            let model = productCategories?[indexPath.row]
+            cell?.filterButton.setTitle(model?.name, for: .normal)
+            cell?.filterButton.tag = model?.id ?? 0
+            cell?.filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+            setButtonColor(cell?.filterButton)
+            
+            return cell ?? UICollectionViewCell()
+        }
+    }
+}
+
+extension MenuViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        productCategories?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell") as? CustomTableViewCell
+        if let categories = productCategories {
+            let model = categories[indexPath.row]
+            let photoUrl = URL(string: model.img)
+            let processor = DownsamplingImageProcessor(size: cell?.productImageView.bounds.size ?? CGSize())
+            |> RoundCornerImageProcessor(cornerRadius: 10)
+            cell?.productImageView.kf.indicatorType = .activity
+            cell?.productImageView.kf.setImage(
+                with: photoUrl,
+                options: [
+                    .processor(processor),
+                    .scaleFactor(UIScreen.main.scale),
+                    .transition(.fade(1)),
+                    .cacheOriginalImage
+                ])
+            cell?.titleLabel.text = model.name
+            cell?.descriptionLabel.text = String(model.id)
+        }
+        return cell ?? UITableViewCell()
     }
 }
--- testTaskMenu/Views/CustomViews.swift
+++ testTaskMenu/Views/CustomViews.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-final class CustomCollectionViewCell: UICollectionViewCell {
+final class AdvertCollectionViewCell: UICollectionViewCell {
     let imageView: UIImageView = {
         let image = UIImageView()
         image.backgroundColor = .white
@@ -20,7 +20,6 @@ final class CustomCollectionViewCell: UICollectionViewCell {
         super.init(frame: frame)
         
         setupView()
-        addSubview(imageView)
         setupConstraints()
     }
     
@@ -32,12 +31,121 @@ final class CustomCollectionViewCell: UICollectionViewCell {
         layer.borderColor = UIColor.lightGray.cgColor
         layer.borderWidth = 3
         layer.cornerRadius = 10
+        addSubview(imageView)
     }
     
     private func setupConstraints() {
         imageView.snp.makeConstraints { make in
             make.top.leading.equalToSuperview().offset(5)
             make.trailing.bottom.equalToSuperview().inset(5)
+        }
+    }
+}
+
+final class FilterCollectionViewCell: UICollectionViewCell {
+    let filterButton: UIButton = {
+        let button = UIButton()
+        button.layer.borderWidth = 0.5
+        button.layer.cornerRadius = 5
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.lightGray, for: .highlighted)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        let buttonColor = UIColor(red: 0.992, green: 0.227, blue: 0.412, alpha: 0.2)
+        filterButton.backgroundColor = buttonColor
+        filterButton.layer.borderColor = buttonColor.cgColor
+        filterButton.setTitleColor(buttonColor.withAlphaComponent(1), for: .normal)
+        filterButton.setTitleColor(buttonColor, for: .highlighted)
+        filterButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        addSubview(filterButton)
+    }
+    
+    private func setupConstraints() {
+        filterButton.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+final class CustomTableViewCell: UITableViewCell {
+    private var backView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.layer.borderWidth = 2
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = .systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    let productImageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .white
+        image.frame.size = CGSize(width: 80, height: 80)
+        return image
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(backView)
+        backView.addSubview(titleLabel)
+        backView.addSubview(descriptionLabel)
+        backView.addSubview(productImageView)
+    }
+    
+    private func setupConstraints() {
+        backView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview().inset(5)
+        }
+        
+        productImageView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(10)
+            make.width.equalTo(80)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.equalTo(productImageView.snp.trailing).offset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.leading.equalTo(productImageView.snp.trailing).offset(20)
         }
     }
 }
